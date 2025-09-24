33a0af359c9c9b2f7a07603e36e5ba79962a97d8
Matteo Arnone <157341387+MatteoArnone@users.noreply.github.com>
2025-09-24 10:28:55 -0400

    Update __init__.py

--- Mint_NM/__init__.py
+++ Mint_NM/__init__.py
@@ -1,1 +1,21 @@
+from .neuralnet_module import init_weights, tanh, tanh_derivative, forward, compute_loss, backward, plot_nn_diagram, init_model, forward_pass, backward_pass, step, reset_model, save_function, change_depth, change_width, draw_network, update_plots
 
+__all__ = [
+  "init_weights",
+  "tanh",
+  "tanh_derivative",
+  "forward",
+  "compute_loss",
+  "backward",
+  "plot_nn_diagram",
+  "init_model",
+  "forward_pass",
+  "backward_pass",
+  "step",
+  "reset_model",
+  "save_function",
+  "change_depth",
+  "change_width",
+  "draw_network",
+  "update_plots",
+]
