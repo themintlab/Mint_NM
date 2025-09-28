b49060cc08a60db2022cacf5ad0ab9d0d7857c64
Matteo Arnone <157341387+MatteoArnone@users.noreply.github.com>
2025-09-28 11:51:57 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Mint_NM',
-    version='0.1.17',
+    version='0.1.18',
     description='A Jupyter widgets for solving',
     url='https://github.com/mwelland/themintlab/Mint_NM',
     author='Joel',
