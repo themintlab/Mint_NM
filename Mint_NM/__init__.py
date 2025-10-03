8c1b215a08ae9201b80119539bb6bfabe5a712a1
Matteo Arnone <157341387+MatteoArnone@users.noreply.github.com>
2025-10-02 22:01:19 -0400

    Update __init__.py

--- Mint_NM/__init__.py
+++ Mint_NM/__init__.py
@@ -1,4 +1,7 @@
 from .neuralnet_module import init_weights, tanh, tanh_derivative, forward, compute_loss, backward, plot_nn_diagram, init_model, forward_pass, backward_pass, step, reset_model, save_function, change_depth, change_width, draw_network, update_plots
+from .openrootfinder_module import RootFinderOpen
+from .closedrootfinder_module import RootFinderClosed
+
 
 __all__ = [
   "init_weights",
@@ -18,4 +21,6 @@ __all__ = [
   "change_width",
   "draw_network",
   "update_plots",
+  "RootFinderOpen",
+  "RootFinderClosed",
 ]
