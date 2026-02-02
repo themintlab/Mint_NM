b0f23fd6822e1d5becb24d4de3ff377728b464f4
Matteo Arnone <157341387+MatteoArnone@users.noreply.github.com>
2026-02-02 18:56:13 -0500

    Add optimization modules to __init__.py

--- Mint_NM/__init__.py
+++ Mint_NM/__init__.py
@@ -1,6 +1,9 @@
 from .neuralnet_module import init_weights, tanh, tanh_derivative, forward, compute_loss, backward, plot_nn_diagram, init_model, forward_pass, backward_pass, step, reset_model, save_function, change_depth, change_width, draw_network, update_plots
 from .openrootfinder_module import RootFinderOpen
 from .closedrootfinder_module import RootFinderClosed
+from .closedoptimization_module import OptimizerClosed
+from .openoptimization_module import OptimizerOpen
+from .gradientoptimization_module import OptimizerGrad
 
 
 __all__ = [
@@ -23,4 +26,7 @@ __all__ = [
   "update_plots",
   "RootFinderOpen",
   "RootFinderClosed",
+  "OptimizerClosed",
+  "OptimizerOpen",
+  "OptimizerGrad",
 ]
